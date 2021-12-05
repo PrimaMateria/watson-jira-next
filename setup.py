0d5f67a29df7f811385a972974ed084944822ec3
matus.benko <matus.benko@gmail.com>
2021-12-05 19:24:21 +0100

    0.3.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="watson_jira_next",
-    version="0.3.0",
+    version="0.3.1",
     description="Format and upload Watson time logs to Jira as Tempo worklogs",
     long_description=readme(),
     long_description_content_type="text/markdown",
